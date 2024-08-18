9f8e6534f575dedc007adeba498db7bfd7c5a219
Matan Yosef <matan.yosef02@gmail.com>
2024-08-18 17:39:06 +1200

    test: added tests for typography and colors

--- src/addon/__tests__/helpers.test.ts
+++ src/addon/__tests__/helpers.test.ts
@@ -4,6 +4,8 @@ import {
     getFontFamiliesAsStrings,
     getPxValue,
     getSubtitle,
+    getTypography,
+    groupTailwindColors,
 } from '../helpers';
 import allColors from 'tailwindcss/colors';
 
@@ -217,5 +219,121 @@ describe('extractFontSizes', () => {
         };
         const result = extractFontSizes(fontSizes);
         expect(result).toEqual({ '2xl': '24px', '3xl': '30px' });
+    });
+});
+
+describe('getTypography', () => {
+    it('should correctly extract and convert font sizes', () => {
+        const fontSizes = { sm: '0.875rem', lg: '1.25rem' };
+        const fontWeights = { normal: '400', bold: '700' };
+        const fontFamilies = { sans: ['Helvetica', 'Arial'] };
+        const result = getTypography(fontSizes, fontWeights, fontFamilies);
+        expect(result.size).toEqual({ sm: '14px', lg: '20px' });
+    });
+
+    it('should correctly convert font families to strings', () => {
+        const fontSizes = { sm: '0.875rem' };
+        const fontWeights = { normal: '400' };
+        const fontFamilies = {
+            sans: ['Helvetica', 'Arial'],
+            serif: ['Georgia', 'Times'],
+        };
+        const result = getTypography(fontSizes, fontWeights, fontFamilies);
+        expect(result.type).toEqual({
+            sans: 'Helvetica, Arial',
+            serif: 'Georgia, Times',
+        });
+    });
+
+    it('should correctly return the typography object', () => {
+        const fontSizes = { sm: '0.875rem' };
+        const fontWeights = { normal: '400' };
+        const fontFamilies = { sans: ['Helvetica', 'Arial'] };
+        const result = getTypography(fontSizes, fontWeights, fontFamilies);
+        expect(result).toEqual({
+            type: { sans: 'Helvetica, Arial' },
+            weight: { normal: '400' },
+            size: { sm: '14px' },
+        });
+    });
+});
+
+describe('groupTailwindColors', () => {
+    it('should group colors where all values are strings', () => {
+        const colors = { red: '#ff0000', blue: '#0000ff' };
+        const result = groupTailwindColors(colors);
+        expect(result).toEqual([
+            { key: 'red', value: { red: '#ff0000' }, subtitle: 'Custom color' },
+            {
+                key: 'blue',
+                value: { blue: '#0000ff' },
+                subtitle: 'Custom color',
+            },
+        ]);
+    });
+
+    it('should group colors where some values are objects', () => {
+        const colors = { red: { 500: '#ff0000' }, blue: '#0000ff' };
+        const result = groupTailwindColors(colors);
+        expect(result).toEqual([
+            { key: 'red', value: { 500: '#ff0000' }, subtitle: 'Custom color' },
+            {
+                key: 'blue',
+                value: { blue: '#0000ff' },
+                subtitle: 'Custom color',
+            },
+        ]);
+    });
+
+    it('should group colors with a mix of strings and objects', () => {
+        const colors = {
+            red: { 500: '#ff0000' },
+            blue: '#0000ff',
+            green: { 300: '#00ff00' },
+        };
+        const result = groupTailwindColors(colors);
+        expect(result).toEqual([
+            { key: 'red', value: { 500: '#ff0000' }, subtitle: 'Custom color' },
+            {
+                key: 'blue',
+                value: { blue: '#0000ff' },
+                subtitle: 'Custom color',
+            },
+            {
+                key: 'green',
+                value: { 300: '#00ff00' },
+                subtitle: 'Custom color',
+            },
+        ]);
+    });
+
+    it('should correctly set the subtitle for default and custom colors', () => {
+        const twReds = {
+            '50': '#fef2f2',
+            '100': '#fee2e2',
+            '200': '#fecaca',
+            '300': '#fca5a5',
+            '400': '#f87171',
+            '500': '#ef4444',
+            '600': '#dc2626',
+            '700': '#b91c1c',
+            '800': '#991b1b',
+            '900': '#7f1d1d',
+            '950': '#450a0a',
+        };
+        const colors = { red: twReds, customColor: '#123456' };
+        const result = groupTailwindColors(colors);
+        expect(result).toEqual([
+            {
+                key: 'red',
+                value: twReds,
+                subtitle: 'Default color from Tailwind CSS',
+            },
+            {
+                key: 'customColor',
+                value: { customColor: '#123456' },
+                subtitle: 'Custom color',
+            },
+        ]);
     });
 });
