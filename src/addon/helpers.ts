6a006049bda0beee82fad8f5e8a86f2e5c1ff798
Matan Yosef <matan.yosef02@gmail.com>
2024-08-18 17:29:52 +1200

    refactor: updated getPxValue to be more generic

--- src/addon/helpers.ts
+++ src/addon/helpers.ts
@@ -1,6 +1,5 @@
 import allColors from 'tailwindcss/colors';
 
-export const TAILWIND_BASE_RATIO = 16;
 /**
  * Group the colors from the resolved Tailwind CSS configuration.
  *
@@ -18,11 +17,11 @@ export const groupTailwindColors = (colors: Record<string, any>) => {
             groupedColors[key] = { [key]: value };
         }
     }
-    // Tranform into an array of objects with key, value, and subtitle
+    // Transform into an array of objects with key, value, and subtitle
     return Object.entries(groupedColors).map(([key, value]) => ({
         key,
         value,
-        subtitle: getSubtitle(key),
+        subtitle: getSubtitle(key, value),
     }));
 };
 
@@ -50,15 +49,15 @@ export const getTypography = (
  * Extract the font sizes from the resolved Tailwind CSS configuration.
  * @param fontSizes
  */
-const extractFontSizes = (
+export const extractFontSizes = (
     fontSizes: Record<string, any>
 ): Record<string, string> => {
     const extractedFontSizes: Record<string, string> = {};
     for (const key in fontSizes) {
         if (Array.isArray(fontSizes[key])) {
-            extractedFontSizes[key] = convertRemToPx(fontSizes[key][0]);
+            extractedFontSizes[key] = getPxValue(fontSizes[key][0]);
         } else {
-            extractedFontSizes[key] = convertRemToPx(fontSizes[key]);
+            extractedFontSizes[key] = getPxValue(fontSizes[key]);
         }
     }
 
@@ -76,8 +75,35 @@ const extractFontSizes = (
     return sortedFontSizesObj;
 };
 
-const convertRemToPx = (rem: string): string => {
-    return `${parseFloat(rem) * TAILWIND_BASE_RATIO}px`;
+// TODO: Add support for other units (or use library which already does this)
+export const TAILWIND_BASE_RATIO = 16;
+const unitConversionRatios: Record<string, number> = {
+    rem: TAILWIND_BASE_RATIO,
+    em: TAILWIND_BASE_RATIO,
+    px: 1,
+    pt: 1.33333, // 1pt = 1.33333px
+    cm: 37.7953, // 1cm = 37.7953px
+    mm: 3.77953, // 1mm = 3.77953px
+    in: 96, // 1in = 96px
+    pc: 16, // 1pc = 16px
+    '%': 0.16, // Assuming 1% of 16px base font size
+};
+export const getPxValue = (size: string): string => {
+    const unitMatch = size.match(/[\d.]+(\D+)$/);
+    if (!unitMatch) {
+        throw new Error(`Invalid size format: ${size}`);
+    }
+
+    const value = parseFloat(size);
+    const unit = unitMatch[1];
+
+    if (!unitConversionRatios[unit]) {
+        throw new Error(`Unsupported unit: ${unit}`);
+    }
+
+    const pxValue = value * unitConversionRatios[unit];
+    const roundedPxValue = parseFloat(pxValue.toFixed(4));
+    return `${roundedPxValue}px`;
 };
 
 export const getFontFamiliesAsStrings = (
